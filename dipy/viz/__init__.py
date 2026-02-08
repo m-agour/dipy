287572a89c7ee9e4d25926cb7df3e6e88d8818ec
m-agour <mo.aggour@gmail.com>
2026-02-08 17:07:00 -0500

    NF: sh_glyph and skyline to dipy.viz

--- dipy/viz/__init__.py
+++ dipy/viz/__init__.py
@@ -1,8 +1,16 @@
-# Init file for visualization package
 import warnings
 
 from dipy.utils.optpkg import optional_package
-from dipy.viz.horizon.app import horizon
+
+try:
+    from dipy.viz.horizon.app import horizon
+except Exception:
+    horizon = None
+
+try:
+    from dipy.viz.horizon.skyline import skyline
+except Exception:
+    skyline = None
 
 fury_pckg_msg = (
     "You do not have FURY installed. Some visualization functions might not "
@@ -10,20 +18,35 @@ fury_pckg_msg = (
     "For detailed installation instructions visit: https://fury.gl/"
 )
 
-# Allow import, but disable doctests if we don't have fury
 fury, has_fury, _ = optional_package(
     "fury", trip_msg=fury_pckg_msg, min_version="0.10.0"
 )
 
 
 if has_fury:
-    from fury import actor, colormap, interactor, lib, shaders, ui, utils, window
-    from fury.data import DATA_DIR as FURY_DATA_DIR, fetch_viz_icons, read_viz_icons
+    from fury import actor, colormap, lib, shaders, ui, utils, window
+
+    try:
+        from fury import interactor
+    except ImportError:
+        interactor = None
+
+    try:
+        from fury.data import (
+            DATA_DIR as FURY_DATA_DIR,
+            fetch_viz_icons,
+            read_viz_icons,
+        )
+    except ImportError:
+        FURY_DATA_DIR = None
+        fetch_viz_icons = None
+        read_viz_icons = None
+
+    from dipy.viz import sh_glyph  # noqa: F401
 
 else:
     warnings.warn(fury_pckg_msg, stacklevel=2)
 
-# We make the visualization requirements optional imports:
 _, has_mpl, _ = optional_package(
     "matplotlib",
     trip_msg="You do not have Matplotlib installed. Some visualization "
